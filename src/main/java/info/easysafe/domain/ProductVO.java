0c3f457da72685eb058528af8e04940c1f1d8830
Jd-han <human@hoho.com>
2016-12-11 18:13:35 +0900

    productVO update

--- src/main/java/info/easysafe/domain/ProductVO.java
+++ src/main/java/info/easysafe/domain/ProductVO.java
@@ -2,57 +2,68 @@ package info.easysafe.domain;
 
 public class ProductVO {
 	
+	private String code;
+	private String seqcode;
 	private String name;
-	private String upc;
-	private String components;
-	private String producer;
 	private String thumimg;
-
-	public String getThumimg() {
-		return thumimg;
+	private String producer;
+	private String barcode;
+	private String type;
+	private String components;
+	
+	public String getCode() {
+		return code;
 	}
-
-	public void setThumimg(String thumimg) {
-		this.thumimg = thumimg;
+	public void setCode(String code) {
+		this.code = code;
 	}
-
+	public String getSeqcode() {
+		return seqcode;
+	}
+	public void setSeqcode(String seqcode) {
+		this.seqcode = seqcode;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getUpc() {
-		return upc;
+	public String getThumimg() {
+		return thumimg;
 	}
-
-	public void setUpc(String upc) {
-		this.upc = upc;
+	public void setThumimg(String thumimg) {
+		this.thumimg = thumimg;
 	}
-
-	public String getComponents() {
-		return components;
-	}
-
-	public void setComponents(String component) {
-		this.components = component;
-	}
-
 	public String getProducer() {
 		return producer;
 	}
-
 	public void setProducer(String producer) {
 		this.producer = producer;
 	}
-
-	@Override
-	public String toString() {
-		return "ProductVO [name=" + name + ", upc=" + upc + ", components=" + components + ", producer=" + producer + "]";
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getComponents() {
+		return components;
+	}
+	public void setComponents(String components) {
+		this.components = components;
 	}
 	
-	
-
+	@Override
+	public String toString() {
+		return "ProductVO [code=" + code + ", seqcode=" + seqcode + ", name=" + name + ", thumimg=" + thumimg
+				+ ", producer=" + producer + ", barcode=" + barcode + ", type=" + type + ", components=" + components
+				+ "]";
+	}
 }
